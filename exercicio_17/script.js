b3e6698277ddababf9729c06f22bf6074352adbb
Hendy17 <hendyvorpagel@gmail.com>
2024-05-31 16:18:08 -0300

    Andamento dos exercícios

---
+++ exercicio_17/script.js
@@ -0,0 +1,7 @@
+function imprimeIdade() {
+   console.log(`você tem ${idade} anos`);
+}
+
+imprimeIdade(12);
+imprimeIdade(28);
+imprimeIdade(45);
---
+++ exercicio_18/script.js
@@ -0,0 +1,7 @@
+function somaDoisNumeros(a,b) {
+   return a + b;
+}
+
+console.log(soma(4,5));
+console.log(soma(10,20));
+console.log(soma(30,50));
